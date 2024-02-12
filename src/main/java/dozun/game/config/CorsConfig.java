442ce8f1bfcd1f377ba8b45e723e770184966530
iamdwn <truongdinhdongduong@gmail.com>
2024-02-12 13:19:05 +0700

    feat: add 'config cors'

---
+++ src/main/java/dozun/game/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package dozun.game.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://bacxiu.hdang09.tech", "http://localhost:5173").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
